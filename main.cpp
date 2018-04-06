aedc27d3e8a85a7ee0961065655b431cb66d2f0a
José Carlos Bedoya Rivera <josecarlos94aqp@gmail.com>
2018-04-06 00:40:52 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -31,13 +31,12 @@ void intercambio(int *a, int * b)
 }
 void multipla_matrices(int a[][], int b[][], int c[][], int n)
 {
-    int i, j, k;
-    for (i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
-        for (j = 0; j < n; j++)
+        for (int j = 0; j < n; j++)
         {
             c[i][j] = 0;
-            for (k = 0; k < N; k++)
+            for (int k = 0; k < n; k++)
                 c[i][j] += a[i][k]*b[k][j];
         }
     }
